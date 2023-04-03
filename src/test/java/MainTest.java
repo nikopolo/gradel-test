4ccc5b05e8f630bfc6cac94868d65b0f5c3cb3ac
nikopolo <bolliboo@yandex.ru>
2023-04-03 22:24:36 +0200

    change firstTest

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class MainTest {
+    @Test
+    void firstTest() {
+        open("qwe");
+    }
+
+}
